d3b69cfac66ce8e5a45b530e05f896d3545c07fd
Joerg Jacobsen <joerg.jacobsen@mac.com>
2015-03-05 10:59:43 +0100

    Do always copy objects in SBPerformSelectorAsync()
    Turns out that it yet does not reliably fetch thumbnails without copying

--- SandBoxingKit/SBUtilities.m
+++ SandBoxingKit/SBUtilities.m
@@ -34,7 +34,7 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 
-#define ALWAYS_COPY_OBJECTS_ON_PERFORM_SELECTOR_ASYNC 0
+#define ALWAYS_COPY_OBJECTS_ON_PERFORM_SELECTOR_ASYNC 1
 
 #pragma mark
 #pragma mark Sandbox Check
